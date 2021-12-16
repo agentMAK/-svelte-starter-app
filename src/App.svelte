5a878dc7e4908b109417f6c63bd4be7f400e9c9d
martin0044 <martin0044@gmail.com>
2021-12-16 15:16:25 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <main>
-	<h1>Hello {name} is REAL!</h1>
+	<h1>Hello {name}</h1>
 	<button on:click={toggleName}>Change name variable</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
